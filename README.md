22ba90341efbc215cef946be4705651b32125278
Dick Davis <dick@hey.com>
2021-11-20 16:06:58 -0600

    Dockerize app

--- README.md
+++ README.md
@@ -1,24 +1,54 @@
 # README
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
+### Local Setup
 
-Things you may want to cover:
+To install, first ensure that you have `docker` and `docker-compose` installed and running, then clone the repo:
 
-* Ruby version
+```
+git clone git@github.com:lvmbdv/starbase.git
+cd starbase
+```
 
-* System dependencies
+Create the local `.env` file and specify any sensitive credentials or other environment variables.
 
-* Configuration
+```
+REDIS_URL=redis://redis:6379/0
+```
 
-* Database creation
+Build the containers.
 
-* Database initialization
+```
+docker-compose build
+```
 
-* How to run the test suite
+Start the containers (detached).
 
-* Services (job queues, cache servers, search engines, etc.)
+```
+docker-compose up -d
+```
 
-* Deployment instructions
+Get a shell in the web container.
 
-* ...
+```
+docker-compose exec web /bin/bash
+```
+
+From here, you'll want to create the database and run any migrations. Please note that you'll want to execute most of your commands from
+a shell within the container.
+
+```
+bundle exec rails db:create
+bundle exec rails db:migrate
+```
+
+Start the server.
+
+```
+bundle exec rails s -b 0.0.0.0
+```
+
+If you need to perform other operations from within the container while the server is running, you can simply get another shell in the web container.
+
+```
+docker-compose exec web /bin/bash
+```
---
+++ db/schema.rb
@@ -0,0 +1,80 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_11_18_215336) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "follows", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "interest_type", null: false
+    t.bigint "interest_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["interest_type", "interest_id"], name: "index_follows_on_interest"
+    t.index ["user_id"], name: "index_follows_on_user_id"
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["title"], name: "index_genres_on_title", unique: true
+  end
+
+  create_table "genres_movies", id: false, force: :cascade do |t|
+    t.bigint "movie_id", null: false
+    t.bigint "genre_id", null: false
+    t.index ["genre_id"], name: "index_genres_movies_on_genre_id"
+    t.index ["movie_id", "genre_id"], name: "index_genres_movies_on_movie_id_and_genre_id", unique: true
+    t.index ["movie_id"], name: "index_genres_movies_on_movie_id"
+  end
+
+  create_table "movies", force: :cascade do |t|
+    t.string "original_title"
+    t.date "release_date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["original_title", "release_date"], name: "index_movies_on_original_title_and_release_date", unique: true
+  end
+
+  create_table "movies_stars", id: false, force: :cascade do |t|
+    t.bigint "movie_id", null: false
+    t.bigint "star_id", null: false
+    t.string "title", null: false
+    t.boolean "is_acting", default: true
+    t.index ["movie_id", "star_id"], name: "index_movies_stars_on_movie_id_and_star_id"
+    t.index ["movie_id"], name: "index_movies_stars_on_movie_id"
+    t.index ["star_id"], name: "index_movies_stars_on_star_id"
+  end
+
+  create_table "stars", force: :cascade do |t|
+    t.string "full_name"
+    t.text "biography"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email_address"
+    t.string "password_digest"
+    t.string "password_reset"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email_address"], name: "index_users_on_email_address", unique: true
+    t.index ["password_reset"], name: "index_users_on_password_reset", unique: true
+  end
+
+  add_foreign_key "follows", "users"
+end
