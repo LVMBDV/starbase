8a7ec0b81a116fd984797ec771f63f99e2754a5f
Ata Kuyumcu <atakuyumcu@fastmail.com>
2021-11-21 18:31:48 +0300

    added imdb dataset and db seeding script

---
+++ db/imdb_dataset/README.md
@@ -0,0 +1,3 @@
+Downloaded from https://www.kaggle.com/stefanoleone992/imdb-extensive-dataset?select=IMDb+title_principals.csv
+Licensed as CC0: Public Domain
+Modified (truncated)
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,67 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require "csv"
+require "date"
+require "json"
+require "progressbar"
+
+movies_by_imdb_id = {}
+
+progress_bar = ProgressBar.create(:title => "Creating movies")
+CSV.foreach(File.join(__dir__, "imdb_dataset", "movies.csv"), headers: true).with_index do |row, index|
+  begin
+    genres = row["genre"].split(",").map { |genre_title| Genre.find_or_create_by(title: genre_title) }
+    movie = Movie.create(original_title: row["original_title"], release_date: Date.parse(row["date_published"]), genres: genres)
+    progress_bar.progress = (index + 1) * 100 / 5000
+    movies_by_imdb_id[row["imdb_title_id"]] = movie.id
+  rescue
+    # life is too short to clean other people's data
+    next
+  end
+end
+
+puts "\nCreated #{Movie.count} movies in #{Genre.count} genres"
+
+stars_by_imdb_id = {}
+
+progress_bar = ProgressBar.create(:title => "Creating stars")
+CSV.foreach(File.join(__dir__, "imdb_dataset", "names.csv"), headers: true).with_index do |row, index|
+  star = Star.create(full_name: row["name"])
+  progress_bar.progress = (index + 1) * 100 / 5000
+  stars_by_imdb_id[row["imdb_name_id"]] = star.id
+end
+
+puts "\nCreated #{Star.count} stars"
+
+progress_bar = ProgressBar.create(:title => "Creating roles")
+CSV.foreach(File.join(__dir__, "imdb_dataset", "roles.csv"), headers: true).with_index do |row, index|
+  movie_id = movies_by_imdb_id[row["imdb_title_id"]]
+  star_id = stars_by_imdb_id[row["imdb_name_id"]]
+
+  unless movie_id.present? && star_id.present?
+    # might have been lost due to truncation
+    next
+  end
+
+  movie = Movie.find(movie_id)
+  star = Star.find(star_id)
+  progress_bar.progress = (index + 1) * 100 / 10000
+
+  unless movie.present? && star.present?
+    # life is too short to clean other people's data
+    next
+  end
+
+  is_acting = ["actor", "actress"].include? row["category"]
+  if is_acting
+    JSON.parse(row["characters"] || "[]").each do |character|
+      Role.create(movie: movie, star: star, title: character, is_acting: true)
+    end
+  else
+    Role.create(movie: movie, star: star, title: row["category"].titleize, is_acting: false)
+  end
+end
+
+puts "\nCreated #{Role.count} roles"
